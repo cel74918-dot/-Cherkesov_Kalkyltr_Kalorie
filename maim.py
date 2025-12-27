31ab259024e0c2741b753e198055cc2984fb30b2
cel74918-dot <250724101+cel74918-dot@users.noreply.github.com>
2025-12-27 21:25:44 +0300

    Создаём функцию

--- maim.py
+++ maim.py
@@ -1,1 +1,4 @@
-print("Kalkyltr_Kalorie")
+def calorie_calculator(protein, fat, carbs): # Калькулятор калорий (Белки, Жиры, Углеводы)
+    calories = 4 * protein + 9 * fat + 4 * carbs # 
+    return calories
+print("Приветствую вас в калькуляторе калорий!")
