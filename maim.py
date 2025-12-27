c81c0745ae97b13f28f9ef4ce2cd88fefafbd458
cel74918-dot <250724101+cel74918-dot@users.noreply.github.com>
2025-12-27 21:30:27 +0300

    Выведем общее количество калорий в продукте

--- maim.py
+++ maim.py
@@ -1,4 +1,10 @@
 def calorie_calculator(protein, fat, carbs): # Калькулятор калорий (Белки, Жиры, Углеводы)
-    calories = 4 * protein + 9 * fat + 4 * carbs # 
+    calories = 4 * protein + 9 * fat + 4 * carbs #
     return calories
 print("Приветствую вас в калькуляторе калорий!")
+protein = float(input("Введите количество белков в граммах: "))
+fat = float(input("Введите количество жиров в граммах: "))
+carbs = float(input("Введите количество углеводов в граммах: "))
+
+total_calories = calorie_calculator(protein, fat, carbs)
+print(f"Общее количество калорий в продукте: total_calories ккал.")
