ca24a16a38e6c144b5714cde93598aae14b51475
cel74918-dot <250724101+cel74918-dot@users.noreply.github.com>
2026-01-12 21:59:41 +0300

    Добавил фрукты с условием

--- maim.py
+++ maim.py
@@ -2,21 +2,53 @@
 def calorie_calculator(protein, fat, carbs): # Калькулятор калорий (Белки, Жиры, Углеводы)
     calories = 4 * protein + 9 * fat + 4 * carbs #
     return calories
-print("Приветствую вас в калькуляторе калорий!")
-protein = float(input("Введите количество белков в граммах: "))
-fat = float(input("Введите количество жиров в граммах: "))
-carbs = float(input("Введите количество углеводов в граммах: "))
+print(f"{"":>25}Приветствую вас в калькуляторе калорий!")
+protein = float(input(f"{"":>5}Введите количество белков в граммах: "))
+fat = float(input(f"{"":>5}Введите количество жиров в граммах: "))
+carbs = float(input(f"{"":>5}Введите количество углеводов в граммах: "))
 
 Final_calories = calorie_calculator(protein, fat, carbs) # Общее количество калорий в еде
 print(f"Общее количество калорий в продукте: {Final_calories} ккал.")
 
-Total = float(input("Введите сколько всего калорий можно потребить")) # В зависимости от диеты!
-kall = 0
-while Total > Final_calories:
-    kall = Total - Final_calories
-    print(f"Отлично для поддержания диеты вам необходимо набрать ещё {kall} ккал!")
-    break
-if Total < Final_calories:
-    print(f"Срочно уберите лишние {Final_calories - Total} ккал, иначе требования не будут соответствовать диете!")
+Total = float(input(f"{"":>5}Введите сколько всего калорий можно потребить: ")) # В зависимости от диеты!
+kkal = 0
+if Total > Final_calories:
+    kkal = Total - Final_calories
+    print(f"Отлично для поддержания диеты вам необходимо набрать ещё {kkal} ккал!")
+elif Total < Final_calories:
+    kkal = Final_calories - Total
+    print(f"Срочно уберите лишние {kkal} ккал, иначе требования не будут соответствовать диете!")
 elif Total == Final_calories:
-    print("У вас идеально сбалансированное питание!")
+    kkal = Total == Final_calories
+    print(f"У вас идеально сбалансированное питание!")
+print(f"{"":>25}Ты хорошо держишься, побалуй себя фруктом. Выбери цифру от 1 до 5, купи и съешь рандомный фрукт(Это не помешает диете!!!).")
+fruits = int(input(f"{"":>5}Введи цифру от 1 до 5 : "))
+if fruits == 1:
+    print(f"{"":>25}Поздравляю ты выиграл яблоко.")
+elif fruits == 2:
+    print(f"{"":>25}Поздравляю ты выиграл грушу.")
+elif fruits == 3:
+    print(f"{"":>25}Поздравляю ты выиграл банан.")
+elif fruits == 4:
+    print(f"{"":>25}Поздравляю ты выиграл персик.")
+elif fruits == 5:
+    print(f"{"":>25}Поздравляю ты выиграл манго.")
+elif fruits != 1 and fruits != 2 and fruits != 3 and fruits != 4 and fruits != 5:
+    print(f"{"":>25} Неверно введена цифра! Пожалуйста введи цифру от 1 до 5")
+print(f"{"":>25}Пожалуйста подождите! Высчитываю ккал учитывая выигранный фрукт.")
+for i in range(fruits):
+    if fruits == 1:
+        i = kkal + 52
+        print(f"С учётом яблока в вашей еде {i} ккал")
+    elif fruits == 2:
+        i = kkal + 57
+        print(f"С учётом груши в вашей еде {i} ккал")
+    elif fruits == 3:
+        i = kkal + 89
+        print(f"С учётом банана в вашей еде {i} ккал")
+    elif fruits == 4:
+        i = kkal + 39
+        print(f"С учётом персика в вашей еде {i} ккал")
+    elif fruits == 5:
+        i = kkal + 60
+        print(f"С учётом манго в вашей еде {i} ккал")
